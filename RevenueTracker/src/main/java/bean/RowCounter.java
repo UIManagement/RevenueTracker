c7c29ded6bd13d80c9daa350ba834e2b9af41d4a
aa <aa@nbkin083b.uldqsin.lan>
2014-06-11 11:20:53 +0530

    added bi.xhtml

--- RevenueTracker/src/main/java/bean/RowCounter.java
+++ RevenueTracker/src/main/java/bean/RowCounter.java
@@ -1,13 +1,10 @@
 package bean;
-
-
 public class RowCounter   {
 
-    private  int row = 0;
+    private transient int row = 0;
 
     public int getRow()
     
-   
     {
     	return ++row-1;
     }
